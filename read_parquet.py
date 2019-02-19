c214e429d8710ea9388bdbda8cfc0bd847fb7b99
David <farfidan@gmail.com>
2019-02-19 14:18:09 -0500

    source_output

--- read_parquet.py
+++ read_parquet.py
@@ -8,5 +8,5 @@ Created on Sun Feb 17 11:04:15 2019
 import pyarrow.parquet as pq
 import pandas as pd
 import fastparquet
-data = pd.read_parquet("combokeywords.parquet", engine='fastparquet')#, engine='pyarrow')
-#data.dtypes
+data = pd.read_parquet("dataset.parquet", engine='fastparquet')#, engine='pyarrow')
+data.set_index("Index", inplace=True)
